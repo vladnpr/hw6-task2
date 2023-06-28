f49c521ff31781f0c678af59148e2808ad99a810
Vladyslav <vladnpr@gmail.com>
2023-06-28 19:53:55 +0300

    main

---
+++ go.mod
@@ -0,0 +1,3 @@
+module go_course/hw6/task2
+
+go 1.20
--- main.go
+++ main.go
@@ -1,1 +1,20 @@
-package task2
+package main
+
+import (
+	"go_course/hw6/task2/route"
+	"go_course/hw6/task2/transport"
+)
+
+func main() {
+	car := transport.Car{}
+	train := transport.Train{}
+	airplane := transport.Airplane{}
+
+	r := route.Route{}
+
+	r.AddVehicle(&car)
+	r.AddVehicle(&train)
+	r.AddVehicle(&airplane)
+	r.ShowVehicles()
+	r.Travel()
+}
